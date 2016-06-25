edeb96f26c7758bc32ba9d3923e1e58da33a3f25
Mattdre <olsen_matthew@yahoo.com>
2016-06-25 09:57:24 -1000

    Making changes to test file, first test passed!

---
+++ js/ShoppingListItem.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function ShoppingListItem( name, description ) {
+  // body...
+}
--- js/test/ShoppingList.spec.js
+++ js/test/ShoppingList.spec.js
@@ -1,13 +1,35 @@
-'use strict';
+// 'use strict';
 
 describe('ShoppingListItem', function(){
 
-  it('should be a class', function(){
-    expect(ShoppingListItem).to.exist;
+  var item;
+  beforeEach( function() {
+    item = new ShoppingListItem('Loco Moco', 'Dank eggs, rice, patty and GRAVYYY');
   });
 
-  it('should have a property named name', function(){
-    expect(ShoppingListItem).to.have.a.property('name');
+  it('ShoppingListItem should be a class', function(){
+    expect(item).to.be.an.instanceOf(ShoppingListItem);
   });
+
+  /////////////
+
+  it('should have a property named name', function(){
+    expect(item).to.have.a.property('name');
+    expect(item.name).to.equal('Loco Moco');
+  });
+
+  it('should have a property named description', function(){
+    expect(ShoppingListItem).to.have.a.property('description');
+  });
+
+  it('should have a property named is_done', function(){
+    expect(ShoppingListItem).to.have.a.property('is_done');
+  });
+
+  it('should have a constructor method that accepts 2 arguments, name and description', function(){
+    ShoppingListItem.should.have.a.constructor;
+  });
+
+  it('should have a   ');
 
 });
