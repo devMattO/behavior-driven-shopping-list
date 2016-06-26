7fa76664527ee61b0e63c635390b192e2612552a
Mattdre <olsen_matthew@yahoo.com>
2016-06-25 20:58:06 -1000

    Finished writing tests and passing tests for ShoppingListItem

--- js/ShoppingListItem.js
+++ js/ShoppingListItem.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function ShoppingListItem( name, description ) {
-  // body...
+  this.name = name;
+  this.description = description;
+  this.is_done = false;
+  this.check = function(){
+    this.is_done = true;
+    return true;
+  };
+  this.uncheck = function() {
+    this.is_done = false;
+    return false;
+  };
+  this.render = function() {
+    var htmlString =`<li class="completed_false">
+  <span>Avocado</span>
+  <span>Must be eaten immediately.</span>
+</li>`;
+
+  return htmlString;
+
+  };
 }
+
+function ShoppingList( items ) {
+  this.items = null;
+
+}
+
+ShoppingList.prototype.itemsConstructor = function(){
+  this.items = {};
+  return this.items;
+};
+
--- js/test/ShoppingList.spec.js
+++ js/test/ShoppingList.spec.js
@@ -7,7 +7,7 @@ describe('ShoppingListItem', function(){
     item = new ShoppingListItem('Loco Moco', 'Dank eggs, rice, patty and GRAVYYY');
   });
 
-  it('ShoppingListItem should be a class', function(){
+  it('should be a class', function(){
     expect(item).to.be.an.instanceOf(ShoppingListItem);
   });
 
@@ -15,21 +15,76 @@ describe('ShoppingListItem', function(){
 
   it('should have a property named name', function(){
     expect(item).to.have.a.property('name');
-    expect(item.name).to.equal('Loco Moco');
   });
 
   it('should have a property named description', function(){
-    expect(ShoppingListItem).to.have.a.property('description');
+    expect(item).to.have.a.property('description');
   });
 
   it('should have a property named is_done', function(){
-    expect(ShoppingListItem).to.have.a.property('is_done');
+    expect(item).to.have.a.property('is_done');
+    expect(item.is_done).to.equal(false);
   });
+
+  //////////////////////
 
   it('should have a constructor method that accepts 2 arguments, name and description', function(){
-    ShoppingListItem.should.have.a.constructor;
+    expect(item.name).to.equal('Loco Moco');
+    expect(item.description).to.equal('Dank eggs, rice, patty and GRAVYYY');
   });
 
-  it('should have a   ');
+  it('should have a method named check', function() {
+    expect(item.check).to.be.a('function');
+  });
+
+  it('calling the instances check method will set its is_done property to true', function() {
+    item.check();
+    expect(item.is_done).to.equal(true);
+  });
+
+  it('should have a method named uncheck', function() {
+    expect(item.uncheck).to.be.a('function');
+  });
+
+  it('calling the instances uncheck method will set its is_done property to false', function() {
+    item.uncheck();
+    expect(item.is_done).to.equal(false);
+  });
+
+  it('should have a method named render', function() {
+    expect(item.render).to.be.a('function');
+  });
+
+  it('should construct and return an html formatted string', function(){
+    expect(item.render()).to.equal(
+`<li class="completed_false">
+  <span>Avocado</span>
+  <span>Must be eaten immediately.</span>
+</li>`);
+  });
+
+
+});
+
+describe('ShoppingList', function(){
+
+  var daList;
+  beforeEach( function() {
+    daList = new ShoppingList();
+  });
+
+  it('should be a class', function(){
+    expect(daList).to.be.an.instanceOf(ShoppingList);
+  });
+
+  it('should have a property named items', function(){
+    expect(daList).to.have.a.property('items');
+  });
+
+  it('should have a constructor method that initializes items as an empty Array', function(){
+    expect(daList.itemsConstructor()).to.equal({});
+  });
+
+
 
 });
